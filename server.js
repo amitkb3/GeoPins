35bfa04f58b1e7cde22c2a06dd3540b789c10c00
Amit Bothra <amit.bothra@outlook.com>
2019-03-03 07:22:45 -0600

    connected Mongodb database to app

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 require('dotenv').config();
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
+  })
+  .then(() => console.log(`DB connected!`))
+  .catch(err => console.error(err));
+
 const server = new ApolloServer({
   typeDefs,
   resolvers
