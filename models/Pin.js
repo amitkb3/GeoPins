41e97a0b457f0805c64857825d2ee9d22b6eab15
Amit Bothra <amit.bothra@outlook.com>
2019-03-04 06:27:40 -0600

    added mongoose model for User & Pin data

---
+++ models/Pin.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const PinSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  image: String,
+  latitude: Number,
+  Longitude: Number,
+  author: { 
+    type: mongoose.Schema.ObjectId,
+    ref: "User"
+  },
+  comments: [
+     {
+       text: String,
+       createdAt: {
+         type: Date,
+         default: Date.now
+       },
+       author: {
+         type: mongoose.Schema.ObjectId,
+         ref: "User"
+       }
+     }
+  ]
+}, { timestamps: true }
+);
+
+module.exports = mongoose.model("Pin", PinSchema);
